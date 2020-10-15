a7df591599a8d3c6c1ec78f241978a6a14af0c6a
RenLishisan <renshisan@qq.com>
2020-10-15 11:04:23 +0800

    完成从数据库拿数据并且按照顺序展示在页面上

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <header className="headerContent">
+                <h1>商品信息展示中心</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/centre.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Centre.css"
+import CardTest from './CardTest'
+
+function Centre() {
+    return (
+        <CardTest/>
+    );
+}
+
+export default Centre;
