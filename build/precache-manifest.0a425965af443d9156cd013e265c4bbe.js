e3977b99f45fd5bcc1c53a9e1f11791ce89eeab8
RenLishisan <renshisan@qq.com>
2020-10-23 11:59:24 +0800

    添加build打包预览文件

---
+++ build/precache-manifest.0a425965af443d9156cd013e265c4bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "9ec942ca3e8bc817174db05c838cb7d8",
+        "url": "/index.html"
+    },
+    {
+        "revision": "8f231465c4bd252c64a0",
+        "url": "/static/css/2.e10332d9.chunk.css"
+    },
+    {
+        "revision": "73058b738dd3d658f6f4",
+        "url": "/static/css/main.861a1132.chunk.css"
+    },
+    {
+        "revision": "8f231465c4bd252c64a0",
+        "url": "/static/js/2.2345ac32.chunk.js"
+    },
+    {
+        "revision": "3453b8997016469371284a28c0e873e2",
+        "url": "/static/js/2.2345ac32.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "73058b738dd3d658f6f4",
+        "url": "/static/js/main.a959f861.chunk.js"
+    },
+    {
+        "revision": "73635f645c271e35ff15",
+        "url": "/static/js/runtime-main.83c3e0c4.js"
+    }
+]);
